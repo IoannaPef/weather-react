833166e8d721c190f31cb6d5513fbecfcf42966e
IoannaKara <ioannakara2009@hotmail.com>
2023-04-06 20:13:16 +0200

    added some backround  color

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props){
     return (
       <ReactAnimatedWeather
         icon={codeMapping[props.code]}
-        color="black"
+        color="pink"
         size={54}
         animate={true}
       />
